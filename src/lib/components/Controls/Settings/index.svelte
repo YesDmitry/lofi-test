f0726ad52acbe6500678424f0e28146ca1e4597e
YesDmitry <124303608+YesDmitry@users.noreply.github.com>
2025-06-06 22:16:59 +0400

    Add files via upload

---
+++ src/lib/components/Controls/Settings/index.svelte
@@ -0,0 +1,97 @@
+<script lang="ts">
+  import { IconSettings } from "@tabler/icons-svelte";
+  import { onMount } from "svelte";
+  import Background from "./Background.svelte";
+  import Volume from "./Volume.svelte";
+
+  let isActive = false;
+
+  function toggle() {
+    isActive = !isActive;
+  }
+
+  // Shortuct to toggle settings with "J" key
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "j") {
+      toggle();
+    }
+  });
+
+  // when mounted toggle settings
+  // to excute settings of children (old saved)
+  onMount(() => {
+    toggle();
+    setTimeout(() => {
+      toggle();
+    }, 10);
+  });
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      isActive &&
+      event.target instanceof HTMLElement &&
+      !event.target.closest("#settings-box")
+    ) {
+      isActive = false;
+    }
+  };
+  document.addEventListener("click", handleClickOutside);
+</script>
+
+<div id="settings-box">
+  <button
+    style={`
+          background-color: ${isActive ? "white" : "transparent"};
+          `}
+    on:click={toggle}
+  >
+    <IconSettings size={25} color={isActive ? "black" : "white"} />
+  </button>
+  {#if isActive}
+    <div class="settings-box">
+      <div>
+        <Background />
+        <Volume />
+        <small>For more information press ESC key</small>
+      </div>
+    </div>
+  {/if}
+</div>
+
+<style>
+  button {
+    color: white;
+    border-radius: 50%;
+    aspect-ratio: 4/4;
+  }
+  .settings-box {
+    position: absolute;
+    left: 0;
+    top: 70px;
+    height: 58vh;
+    padding: 5px 20px;
+    width: 340px; /* Like controls width */
+    color: white;
+    border-radius: 20px;
+    backdrop-filter: blur(10px);
+    background-color: rgba(0, 0, 0, 40%);
+    overflow-y: auto;
+    animation: show 0.4s ease-in-out;
+  }
+  @keyframes show {
+    from {
+      transform: translateY(-10%);
+      opacity: 0;
+    }
+    to {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+  small {
+    display: block;
+    font-size: x-small;
+    margin-top: 30px;
+    color: azure;
+  }
+</style>
---
+++ src/lib/components/Controls/index.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import CampFire from "./CampFire/index.svelte";
+  import Jungle from "./Jungle/index.svelte";
+  import Rain from "./Rain/index.svelte";
+  import Settings from "./Settings/index.svelte";
+  import Thunder from "./Thunder/index.svelte";
+
+  const STORAGE_KEY = "Volumes";
+  const DEFFAULT_VOLUMES = {
+    rain: 1,
+    thunder: 1,
+    campfire: 1,
+    jungle: 1,
+    main_track: 1,
+  };
+  // Load previous vols or defualt
+  let volumes =
+    JSON.parse(localStorage.getItem(STORAGE_KEY)) || DEFFAULT_VOLUMES;
+
+  // Update
+  setInterval(() => {
+    volumes = volumes =
+      JSON.parse(localStorage.getItem(STORAGE_KEY)) || DEFFAULT_VOLUMES;
+  }, 200);
+</script>
+
+<div class="controls">
+  <Rain volume={volumes.rain} />
+  <Thunder volume={volumes.thunder} />
+  <Jungle volume={volumes.jungle} />
+  <CampFire volume={volumes.campfire} />
+  <Settings />
+</div>
+
+<style>
+  .controls {
+    width: 340px;
+    height: 50px;
+    color: white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    border-radius: 50px;
+    backdrop-filter: blur(2px);
+    background-color: rgba(0, 0, 0, 28%);
+  }
+</style>
